5ad850edf1d1a53458b7e02462a249d523dd9668
sumanmallaz <sumanmallaz46@gmail.com>
2020-09-05 12:43:57 +0200

    products cloned,initial values set

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-//fetch data
+// fetch data
 fetch("https://kea-alt-del.dk/t5/api/productlist")
-    .then(function(response){
-    console.log(response);
+  .then(function (response) {
+    console.log(response)
     return response.json();
-}).then(function(data){
-    console.log(data);
-    document.querySelector("#demoo").innerHTML = data[0].id;
-    document.querySelector("#demooo").innerHTML = data[0].category;
-});
+  })
+  .then(function (data) {
 
-document.querySelector("#demo").innerHTML = "Hello World!";
+    dataReceived(data);
+  })
 
+function dataReceived(products) {
+  //loop through products
+  products.forEach(showProduct)
+}
+
+//executed once for each product
+function showProduct(myProduct) {
+  console.log(myProduct)
+  //finding the template
+  const temp = document.querySelector("#productTemplate").content;
+  //clone the template
+  const myCopy = temp.cloneNode(true);
+  //fill out the template
+  myCopy.querySelector(".data_name").textContent = myProduct.name;
+  //append
+  const parentElem = document.querySelector("section#starter");
+  parentElem.appendChild(myCopy)
+}
+
+
+
+/*
+const name1 = "Jonas";
+
+function hi() {
+  const name2 = "Lasse";
+  console.log(name1, name2)
+}
+
+console.log(name1, name2)
+*/
